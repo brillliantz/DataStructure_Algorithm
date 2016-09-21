d64ea568d73a6a748effca1d6e7cfec36a56e83e
brillliantz <brillliantz@outlook.com>
2016-09-21 09:20:54 +0800

    new message

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -6,5 +6,6 @@ package src.com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("new mes");
     }
 }
